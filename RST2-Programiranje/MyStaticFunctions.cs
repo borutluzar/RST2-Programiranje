daaa7f059b21409486d83335e812fdb3ce451961
Borut Lužar <borut.luzar@gmail.com>
2020-10-28 23:26:43 +0100

    Add project files.

---
+++ RST2-Programiranje/MyStaticFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RST2_Programiranje
+{
+    class MyStaticFunctions
+    {
+        /// <summary>
+        /// Counts all prime numbers up to n
+        /// </summary>
+        /// <param name="n">An integer up to which we count primes</param>
+        public static int CountPrimes(int n)
+        {
+            int countPrimes = 0;
+
+            // For each i between 1 and n check if it is a prime number
+            for(int i=2; i<=n; i++)
+            {
+                // Count number of divisors of the current number i
+                int countDivisors = 0;
+                for(int j=2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                        countDivisors++;
+
+                    if (countDivisors > 0)
+                        break;
+                }
+
+                if (countDivisors == 0)
+                    countPrimes++;
+            }
+
+            return countPrimes;
+        }
+    }
+}
---
+++ RST2-Programiranje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RST2_Programiranje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            PrepareOutputs();                        
+            Console.Read();
+        }
+
+        static void PrepareOutputs()
+        {
+            int n = 22;
+            // Klic funkcije iz drugega razreda
+            int primes = MyStaticFunctions.CountPrimes(n);
+            // Običajen izpis
+            Console.WriteLine("Število praštevil med 1 in "+ n +" je " + primes + ".");
+            Console.WriteLine("Število praštevil med 1 in {0} je {1}.", n, primes);
+
+            // Interpolacija nizov - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+            Console.WriteLine($"Število praštevil med 1 in {n} je {primes}.");
+
+            // 
+            Console.WriteLine($"|{"Ime in priimek",-20}|{"Ocena",5}|");
+            Console.WriteLine($"|{"Borut Lužar",-20}|{"6",5}|");
+        }
+    }
+}
